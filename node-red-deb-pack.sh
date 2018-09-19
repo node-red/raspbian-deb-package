6e0b5f7556e878f2fe965afba5ab758f7d485d48
Dave Conway-Jones <conway@uk.ibm.com>
2018-09-19 10:02:39 +0100

    remove the work PI from script variables
    bump to 0.19.4

--- node-red-deb-pack.sh
+++ node-red-deb-pack.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VER=0.19.3
+VER=0.19.4
 
 cd /usr/lib/node_modules
 sudo find . -type f -name .DS_Store -exec rm {} \;
